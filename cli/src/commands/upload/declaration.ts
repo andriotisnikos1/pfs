620d1e1fe0b1d4f1f5b1b6169758f279aefdd275
Nikos Andriotis <67287051+andriotisnikos1@users.noreply.github.com>
2024-03-16 14:08:48 +0200

    clear main branch

--- cli/src/commands/upload/declaration.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from "commander";
-
-const upload = new Command("upload");
-
-upload
-    .description("Upload a file to PFS")
-    .argument("<file>", "The file to upload")
-    .option("-d, --destination <destination>", "The destination path")
