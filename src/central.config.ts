d2d4342cb26b2d03db56b942189b8248e61dc952
Yololeio <67287051+andriotisnikos1@users.noreply.github.com>
2024-04-08 16:44:36 +0300

    fixes

--- src/central.config.ts
+++ src/central.config.ts
@@ -1,17 +1,16 @@
 import "colors"
 import getConfig from "./lib/root/getConfig.js";
 import { log } from "@clack/prompts";
-import { argv } from "process";
 
 
 export const config = await getConfig()
 
-if (!process.argv.join(" ").includes("pfs hosts") || !process.argv.includes("add")) 
-log.info("Using host: " + config.server.friendlyName.yellow + " " + `(${config.server.host})`.cyan)
+if (!process.argv.join(" ").includes("pfs hosts") && config.server)
+    log.info("Using host: " + config.server.friendlyName.yellow + " " + `(${config.server.host})`.cyan)
 
 export const serverURL = (() => {
-    if (process.argv.join(" ").includes("pfs hosts")) return ""
-   if (config.server.protocol === "https" && config.server.port === 443) return `https://${config.server.host}`
+    if (!config.server) return ""
+    if (config.server.protocol === "https" && config.server.port === 443) return `https://${config.server.host}`
     if (config.server.protocol === "http" && config.server.port === 80) return `http://${config.server.host}`
     return `${config.server.protocol}://${config.server.host}:${config.server.port}`
 })()
