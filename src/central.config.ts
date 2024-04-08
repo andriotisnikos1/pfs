befea62760e4c56f080db01be792111beaa56e14
Nikos Andriotis <67287051+andriotisnikos1@users.noreply.github.com>
2024-04-08 16:42:14 +0300

    c

--- src/central.config.ts
+++ src/central.config.ts
@@ -1,11 +1,12 @@
 import "colors"
 import getConfig from "./lib/root/getConfig.js";
 import { log } from "@clack/prompts";
+import { argv } from "process";
 
 
 export const config = await getConfig()
 
-if (!process.argv.join(" ").includes("pfs hosts")) 
+if (!process.argv.join(" ").includes("pfs hosts") || !process.argv.includes("add")) 
 log.info("Using host: " + config.server.friendlyName.yellow + " " + `(${config.server.host})`.cyan)
 
 export const serverURL = (() => {
